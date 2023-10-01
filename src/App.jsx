abbdf0cf8b9aebbec0765b0532858df0c3e0713a
Glendy <glendytuyuc@gmail.com>
2023-10-01 13:43:41 -0600

    agregar componente listadoNoticias y Noticia

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,36 @@
 import { Container, Grid, Typography } from "@mui/material"
 import Formulario from "./components/Formulario"
+import ListadoNoticias from "./components/ListadoNoticias"
+import { NoticiasProvider } from "./context/NoticiasProvider"
 
 function App() {
 
-
   return (
-    <Container>
-      <header>
-        <Typography align="center" marginY={5} component="h1"
-         variant="h3"
-        >
-          Buscador de noticias
-        </Typography>
-      </header>
+    <NoticiasProvider> 
+      <Container>
+        <header>
+          <Typography align="center" marginY={5} component="h1"
+          variant="h3"
+          >
+            Buscador de noticias
+          </Typography>
+        </header>
 
-      <Grid
-       container
-       direction="row"
-       justifyContent="center"
-       alignItems="center"
-      > 
-        <Grid item xs={12} md={6} lg={4}> 
-          <Formulario/>
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+        > 
+          <Grid item xs={10} md={6} lg={4}> 
+            <Formulario/>
+          </Grid>
         </Grid>
-      </Grid>
-    </Container>
+
+        <ListadoNoticias />
+
+      </Container>
+    </NoticiasProvider>
   )
 }
 
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -5,6 +5,7 @@ import {
   MenuItem, 
   Button, 
   Box } from "@mui/material"
+import useNoticias from "../hooks/useNoticias"
 
   const CATEGORIAS = [
     { value: 'general', label: 'General'},
@@ -18,12 +19,17 @@ import {
   
 
 const Formulario = () => {
+ 
+   const {categoria, handleChangeCategoria } = useNoticias()
+
   return (
     <form>
       <FormControl fullWidth>
         <InputLabel>Categoria</InputLabel>
         <Select
          label="Categoria"
+         onChange={handleChangeCategoria}
+         value={categoria}
         >
           {CATEGORIAS.map(categoria => (
             <MenuItem
